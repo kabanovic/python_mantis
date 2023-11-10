6de3226f698bcede3cd42d9d5b534cb75bbcd491
kabanovic <akim_l.a@mail.ru>
2023-11-11 01:07:33 +0300

    начало

---
+++ fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from fixture.session import Session
+from selenium.webdriver.common.by import By
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = Session(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if wd.current_url.endswith("/index.php"):
+            return
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
